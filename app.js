f902eff869f45203ddb9fd66b3557d3d97b2dac5
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-25 22:44:47 +0530

    first push

---
+++ app.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import {
+  analyzeGivenFood,
+  createDietPlan,
+  createDietPlanForBadFood,
+} from "./utils/ai.js";
+const app = express();
+
+app.get("/", async (req, res) => {
+  const resForDiet = await createDietPlan(
+    "I am 6'2 and I weight 82kg, I want lose fat and gain muscles. I am vegeterian and I do intense workouts "
+  );
+
+  const dietPlan = resForDiet.lc_kwargs.content;
+  // Analyzing Food
+  const resForAnalyze = await analyzeGivenFood("I ate cheesecake today.");
+  const calorie = resForAnalyze.lc_kwargs.content;
+  const resForBadFood = await createDietPlanForBadFood(
+    `Current Diet: ${dietPlan} \n Food eaten today: ${calorie.foodEaten} \n Diet Preference: ${dietPlan.dietPreference}`
+  );
+
+  const dietPlanForBadFood = resForBadFood.lc_kwargs.content;
+
+  console.log(dietPlan);
+  console.log(calorie);
+  console.log(dietPlanForBadFood);
+  res.send("Done");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
