ed2d6a57889ea134bad398f62b045a4534050516
Vivek Shukla <vivekshuklahere@gmail.com>
2024-03-01 12:59:52 +0530

    added react toastify

--- app/api/create-diet/route.ts
+++ app/api/create-diet/route.ts
@@ -1,21 +1,35 @@
 import { dietFormSchema } from "@/utils/validation";
 import { NextResponse } from "next/server";
 import { createDietPlan } from "@/utils/ai";
+import { z } from "zod";
 
 export const POST = async (request: Request) => {
-  const userData = await request.json();
-  const userDataParsed = dietFormSchema.parse(userData);
+  try {
+    const userData = await request.json();
+    userData.age = parseInt(userData.age);
+    userData.weight = parseInt(userData.weight);
+    userData.height = parseInt(userData.height);
+    const userDataParsed = dietFormSchema.parse(userData);
 
-  const resForDiet = await createDietPlan(JSON.stringify(userDataParsed));
+    const resForDiet = await createDietPlan(JSON.stringify(userDataParsed));
 
-  const dietPlan = resForDiet.lc_kwargs.content;
+    const dietPlan = resForDiet.lc_kwargs.content;
 
-  const jsonStr = dietPlan.slice(
-    dietPlan.indexOf("{"),
-    dietPlan.lastIndexOf("}") + 1
-  );
+    const jsonStr = dietPlan.slice(
+      dietPlan.indexOf("{"),
+      dietPlan.lastIndexOf("}") + 1
+    );
 
-  const jsonObj = JSON.parse(jsonStr);
+    const jsonObj = JSON.parse(jsonStr);
 
-  return NextResponse.json(jsonObj);
+    return NextResponse.json({ message: jsonObj });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      // If it's a ZodError, extract the error messages
+      const errorMessages = error.errors.map((e) => e.message);
+      return NextResponse.json({ errors: errorMessages });
+    }
+
+    return NextResponse.json({ error });
+  }
 };
--- utils/validation.ts
+++ utils/validation.ts
@@ -1,22 +1,36 @@
 import { z } from "zod";
+import { Gender, ActivityLevel } from "./enums";
 
 export const dietFormSchema = z.object({
   age: z
-    .number()
-    .min(18, { message: "Must be 18 or older" })
-    .max(120, { message: "Seems a bit high..." }),
+    .number({
+      required_error: "Age is required!",
+      invalid_type_error: "Age should be number!",
+    })
+    .min(18, { message: "Age must be 18 or older" })
+    .max(120, { message: "Age seems a bit high..." }),
 
-  gender: z.enum(["male", "female"]),
+  gender: z.enum([Gender.MALE, Gender.FEMALE]),
 
-  weight: z.number().positive({ message: "Weight must be positive" }),
+  weight: z
+    .number({
+      required_error: "Weight is required",
+      invalid_type_error: "Weight should be number!",
+    })
+    .positive({ message: "Weight must be positive" }),
 
-  height: z.number().positive({ message: "Height must be positive" }),
+  height: z
+    .number({
+      required_error: "Height is required!",
+      invalid_type_error: "Height should be number!",
+    })
+    .positive({ message: "Height must be positive" }),
 
   activityLevel: z.enum([
-    "Sedentary",
-    "Lightly Active",
-    "Moderately Active",
-    "Very Active",
+    ActivityLevel.SEDENTARY,
+    ActivityLevel.LIGHTLY_ACTIVE,
+    ActivityLevel.MODERATELY_ACTIVE,
+    ActivityLevel.VERY_ACTIVE,
   ]),
 
   dietaryPreferences: z.string().optional(),
